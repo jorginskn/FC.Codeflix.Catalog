641d239c8b4a10ff051744e0a6f8b95e252170bb
Jorge Martins <77112497+jorginskn@users.noreply.github.com>
2026-07-14 11:54:54 -0300

    Reorganiza estrutura e adiciona suporte ao MediatR
    Reorganiza os arquivos do projeto para uma nova estrutura de
    diretórios (`src` e `tests`). Adiciona o pacote `MediatR` para
    gerenciar casos de uso, incluindo a configuração inicial para
    `CreateCategory`.
    
    Ajusta referências nos arquivos `.csproj` para refletir a nova
    estrutura e remove dependências desnecessárias. Adiciona novos
    `ProjectReference` para garantir a integração entre os projetos.

---
+++ src/FC.Codeflix.Catalog.Api/Configurations/UseCasesConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+using MediatR;
+
+namespace FC.Codeflix.Catalog.Api.Configurations;
+
+public static class UseCasesConfiguration
+{
+    public static IServiceCollection AddUseCases(this IServiceCollection services)
+    {
+        services.AddMediatR(typeof(CreateCategory));
+        return services;
+    }
+}
--- tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTestFixture.cs
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTestFixture.cs
@@ -1,5 +1,4 @@
 ﻿using FC.Codeflix.Catalog.Domain.Entity;
-using FC.Codeflix.Catalog.Domain.Repository;
 using FC.Codeflix.Catalog.Infra.Data.EF;
 using FC.Codeflix.Catalog.IntegrationTests.Base;
 using Microsoft.EntityFrameworkCore;
