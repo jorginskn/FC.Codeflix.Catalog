2ff61b87e3fa84fc840e2d4f8485a599e1438a8b
Jorge Martins <77112497+jorginskn@users.noreply.github.com>
2026-07-22 11:58:04 -0300

    Adiciona CategoriesController e remove WeatherForecast
    - Adicionada a extensão `AddInfra` no método `AddUseCases`
      para registrar serviços adicionais na injeção de dependência.
    - Criada a classe `CategoriesController` com endpoint `POST`
      para criação de categorias, utilizando o padrão Mediator.
    - Removidas as classes `WeatherForecastController` e
      `WeatherForecast`, incluindo o endpoint de exemplo.
    - Atualizada a versão do pacote `Microsoft.EntityFrameworkCore.InMemory`
      de `9.0.0` para `9.0.18` no projeto de testes de integração.

--- src/FC.Codeflix.Catalog.Api/Configurations/UseCasesConfiguration.cs
+++ src/FC.Codeflix.Catalog.Api/Configurations/UseCasesConfiguration.cs
@@ -1,5 +1,5 @@
 ﻿using FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
-
+using FC.Codeflix.Catalog.Infra.Data.EF.Extensions.DependencyInjection;
 
 namespace FC.Codeflix.Catalog.Api.Configurations;
 
@@ -8,6 +8,7 @@ public static class UseCasesConfiguration
     public static IServiceCollection AddUseCases(this IServiceCollection services)
     {
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<CreateCategory>());
+        services.AddInfra();
         return services;
     }
 }
